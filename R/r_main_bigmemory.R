24be4e3895a01af332cf13238034496a6bfbed35
SCOTT COGGESHALL <scott.coggeshall@va.gov>
2020-01-07 12:09:09 -0800

    create r_main_bigmemory and worker_bigmemory; functions for doing distributed ADMM with data stored in big.matrix format to avoid having to pass large objects to workers

---
+++ R/r_main_bigmemory.R
@@ -0,0 +1,24 @@
+r_main_bigmemory <- function(){
+  
+  
+  
+  
+  
+  
+}
+
+
+worker_bigmemory <- function(dat_desc, inverse_desc, constraint_desc, indices){
+  
+  dat <- bigmemory::attach.big.matrix(bm_desc)
+  
+  inverse <- bigmemory::attach.big.matrix(inverse_desc)
+  
+  constraints <- bigmemory::attach.big.matrix(constraint_desc)
+  
+  
+  
+  
+  
+  
+}
