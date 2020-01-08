dbca4f4029cecd96cb7d08737d2924947389c92b
SCOTT COGGESHALL <scott.coggeshall@va.gov>
2020-01-08 11:25:53 -0800

    add body to worker_bigmemory function

--- R/r_main_bigmemory.R
+++ R/r_main_bigmemory.R
@@ -1,24 +1,40 @@
 r_main_bigmemory <- function(){
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
 }
 
 
-worker_bigmemory <- function(dat_desc, inverse_desc, constraint_desc, indices){
-  
+worker_bigmemory <- function(dat_desc, inverse_desc, constraint_desc,
+ beta_desc, eta_desc, i){
+
+  dat_indices <- if(i != M) ((i - 1)*M + 1):(i*M) else ((i - 1)*M + 1):n
+  inverse_indices <- ((i - 1)*p + 1):(i*p)
   dat <- bigmemory::attach.big.matrix(bm_desc)
-  
+  beta_mat <- bigmemory::attach.big.matrix(beta_desc)
+  eta_mat <- bigmemory::attach.big.matrix(eta_desc)
   inverse <- bigmemory::attach.big.matrix(inverse_desc)
-  
+
   constraints <- bigmemory::attach.big.matrix(constraint_desc)
-  
-  
-  
-  
-  
-  
+
+  xbeta <- alpha*dat[dat_indices,2:ncol(dat) ]%*%beta_mat[, i] +
+            (1 - alpha)*(dat[dat_indices, 1] - constraints[dat_indices, 1])
+
+  constraints[dat_indices, 1] <- shrink(constraints[dat_indices, 2]/rhon +
+
+  dat[dat_indices,1] - xbeta - .5*(2*tau - 1)/(n*rhon),
+    .5*rep(1, length(dat_indices))/(n*rhon))
+
+  beta_mat[, i] <- inverse[inverse_indices,]%*%(t(dat[dat_indices, -1])%*%dat[dat_indices, 1] - constraints[dat_indices, 1] +
+    constraints[dat_indices, 2]/rhon) - eta_mat[, i]/rhon + beta_global_i)
+
+  constraints[dat_indices, 2] <- constraints[dat_indices, 2] + rhon*(dat[dat_indices, 1] - xbeta - constraints[dat_indices, 1])
+
+  eta_mat[, i] <- eta_mat[, i] + rhon*(beta_mat[, i] - beta_global_i)
+
+  NULL
+
 }
